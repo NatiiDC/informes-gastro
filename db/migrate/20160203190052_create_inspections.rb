1f4eccd77ba7e9ee7379353bda22af7e01031cb0
NatiiDC <nataliaeaparicio@gmail.com>
2016-02-20 00:51:04 -0300

    Sigue en proceso el modelo Inspection

--- db/migrate/20160203190052_create_inspections.rb
+++ db/migrate/20160203190052_create_inspections.rb
@@ -1,13 +1,16 @@
 class CreateInspections < ActiveRecord::Migration
   def change
     create_table :inspections do |t|
-      t.string :name
-      t.string :type
+      t.string :study_of_name
+      t.string :study_of_type
       t.date :date
       t.string :reason
       t.string :report
       t.string :conclusion
       t.string :diagnostic
+      t.string :stomach
+      t.string :esophagus
+      t.string :duodenum
 
       t.references :patient, index: true
 
