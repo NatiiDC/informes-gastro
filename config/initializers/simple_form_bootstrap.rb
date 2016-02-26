ce44b9ee180b71960a0241afc25dd040749b468f
NatiiDC <nataliaeaparicio@gmail.com>
2016-02-25 23:58:06 -0300

    view new inspection - simple_formg - ok

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -3,13 +3,11 @@ SimpleForm.setup do |config|
   config.wrappers :standar, tag: 'div', class: 'form-group', error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
-    b.use :label, class: 'control-label'
+    b.use :label, class: 'col-md-4 control-label'
 
     b.wrapper tag: 'div' do |ba|
-      ba.wrapper tag: 'div', class: 'input-group col-sm-12' do |append|
-        append.use :input, class: 'form-control'
-      end
-      ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
+      ba.use :input, class: 'form-control'
+    ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
     end
   end
 
