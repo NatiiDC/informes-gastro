c3518837c70188b5e410c8ee937a40ec845d3a77
NatiiDC <nataliaeaparicio@gmail.com>
2016-03-01 15:25:02 +0000

    fix error when you want to load any image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,17 +4,22 @@ class ImagesController < ApplicationController
     @inspection = Inspection.find(params['inspection_id'])
 
     @next = true
-    image_params["capture"].each do |capture|
-      @image = @inspection.images.new({"name" => image_params["name"]})
-      @image.capture = capture
-      if ! @image.save
-        @next = false
-        break
+    if ! image_params["capture"].nil?
+      image_params["capture"].each do |capture|
+        @image = @inspection.images.new({"name" => image_params["name"]})
+        @image.capture = capture
+        if ! @image.save
+          @next = false
+          break
+        end
       end
+      @notice = 'La/s imagen/es se ha/n guardado'
+    else
+      @notice = "No ha seleccionado ninguna imagen"
     end
 
     if @next
-      redirect_to :back, notice: 'La/s imagen/es se ha/n guardado'
+      redirect_to :back, notice: @notice
     else
       render :back
     end
